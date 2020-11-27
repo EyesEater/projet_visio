3bc20e54f6162fa7b50fbfe1a2c9b2b0df20868b
Xavier <saga.bomb09@gmail.com>
2020-11-27 18:24:55 +0100

    Fix issue with treemap

--- script/treemap.js
+++ script/treemap.js
@@ -80,93 +80,6 @@ async function treemap(artistes, genres) {
         }
     }
 
-    const renderLeaf = data => {
-        const root = treemap(data);
-
-        const svg    = d3.select('.treemap');
-        const newSvg = d3.select('.temp')
-            .attr('viewBox', [0, 0, width, height]);
-
-        // Create shadow
-        newSvg.append('filter')
-            .attr('id', 'shadow')
-            .append('feDropShadow')
-            .attr('flood-opacity', 0.5)
-            .attr('dx', 0)
-            .attr('dy', 0)
-            .attr('stdDeviation', 2);
-
-        // Create node
-        const node = newSvg.selectAll('g')
-            .data(d3.group(root, d => d.height))
-            .join('g')
-            .attr('filter', 'url(#shadow)')
-            .selectAll('g')
-            .data(d => d[1])
-            .join('g')
-            .attr('transform', d => `translate(${d.x0},${d.y0})`);
-
-        // Create title
-        node.append('title')
-            .text(d => {
-                return d.name;
-            });
-
-        // Create rectangle
-        node.append('rect')
-            .attr('id', d => d.nodeId = uuidv4())
-            .attr('fill', d => d.data.color)
-            .attr('width', d => d.x1 - d.x0)
-            .attr('height', d => d.y1 - d.y0);
-
-        // Create clip path for text
-        node.append('clipPath')
-            .attr('id', d => d.clipId = uuidv4())
-            .append('use')
-            .attr('href', d => `#${d.nodeId}`);
-
-        // Create labels
-        node.append('text')
-            .attr('clip-path', d => `url(#${d.clipId})`)
-            .selectAll('tspan')
-            .data(d => [
-                d.data.name,
-                d.data.gender ? ( (d.data.gender === "Female") ? "Femme" : "Homme" ) : "Groupe",
-                d.data.genres,
-                (d.data.type === "Group") ? "Date de création du groupe " + d.data.lifeSpan.begin : "Date de naissance " + d.data.lifeSpan.begin,
-                (d.data.type === "Group") ? ( (d.data.lifeSpan.end) ? "Date de fin du groupe " + d.data.lifeSpan.end : "Le groupe existe toujours"): ((d.data.lifeSpan.end) ? "Dates de décès " + d.data.lifeSpan.end : "L'artiste n'est pas décédé"),
-                d.data.location.city + ", " + d.data.location.country,
-                "Nombre de fans deezer " + d.data.deezerFans
-            ])
-            .join('tspan')
-            .attr('x', 3)
-            .attr('y', (d, i, nodes) => (i + 1) * 20)
-            .text(d => d);
-
-        // Fade out old svg
-        svg.transition()
-            // .ease(d3.easeCubicIn)
-            .duration(animationSpeed)
-            .attrTween('opacity', () => d3.interpolate(1, 0))
-
-        // Fade in new svg
-        newSvg.transition()
-            // .ease(d3.easeCubicOut)
-            .duration(animationSpeed)
-            .attrTween('opacity', () => d3.interpolate(0, 1))
-            .attr('class', 'treemap')
-            .on('end', () => {
-                // At the very end, swap classes and remove everything from the temporary svg
-                svg.attr('class', 'temp').selectAll('*').remove();
-            });
-
-        d3.select('select').on('change', function () {
-            color = d3.scaleSequential([8, 0], d3[d3.select(this).property('value')]);
-
-            node.select('rect').attr('fill', d => d.data.color);
-        });
-    };
-
     const render = data => {
         const root = treemap(data);
         var popup = d3.select("body").append("div")
@@ -266,25 +179,11 @@ async function treemap(artistes, genres) {
                 popup.html("")
                     .style("left", ("0 px"))
                     .style("top", ("0 px"));
-            })
-            //.on('click', (e,d) => displayLeaf(d))
-
-        // Fade out old svg
-        svg.transition()
-            // .ease(d3.easeCubicIn)
-            .duration(animationSpeed)
-            .attrTween('opacity', () => d3.interpolate(1, 0))
-
-        // Fade in new svg
-        newSvg.transition()
-            // .ease(d3.easeCubicOut)
-            .duration(animationSpeed)
-            .attrTween('opacity', () => d3.interpolate(0, 1))
-            .attr('class', 'treemap')
-            .on('end', () => {
-                // At the very end, swap classes and remove everything from the temporary svg
-                svg.attr('class', 'temp').selectAll('*').remove();
             });
+
+        newSvg.attr('class', 'treemap');
+
+        svg.attr('class', 'temp').selectAll('*').remove();
 
         d3.select('select').on('change', function () {
             //color = d3.scaleSequential([8, 0], d3[d3.select(this).property('value')]);
@@ -297,7 +196,7 @@ async function treemap(artistes, genres) {
 
         render(data);
 
-        d3.select('button').on('click', () => render(data));
+        d3.select('#zoomOut').on('click', () => render(data));
     })();
 
     return "Fin Treemap";
